4b3b4aefc80bd5862f6402fa09fb8e67a0d544a1
nguyenthaison <nguyenthaison9193@gmail.com>
2017-03-15 08:58:03 +0700

    init react

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require("webpack");
+
+const paths = {
+  js: __dirname + "/app/assets/javascripts",
+  jsx: __dirname + "/app/jsx",
+}
+
+module.exports = {
+  entry: paths.jsx + "/index.jsx",
+  output: {
+    path: paths.js,
+    filename: "react-app.js",
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: [
+        "babel?cacheDirectory",
+      ],
+    }]
+  },
+  plugins: [
+     new webpack.ProvidePlugin({
+        React: "react",
+        update: "react-addons-update",
+        mui: "material-ui",
+        t: "counterpart",
+        config: __dirname + "/config/react_app",
+        cm: paths.jsx + "/Common",
+        BaseComponent : paths.jsx + "/BaseComponent",
+        PageComponent : paths.jsx + "/PageComponent",
+        BaseMaster: paths.jsx + "/Master/BaseMaster",
+     })
+  ],
+}
