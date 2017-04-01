a2a9e8117f09f97972cb1b402b2059b945d1e781
nguyenthaison <nguyenthaison9193@gmail.com>
2017-04-01 10:47:24 +0700

    home page continous

--- app/jsx/Songs/Song.jsx
+++ app/jsx/Songs/Song.jsx
@@ -219,6 +219,18 @@ export default class Song extends PageComponent {
 
     return (
       <div className="song-play">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" className="equilizer" viewBox="0 0 128 128">
+            <g>
+              <title>Audio Equilizer</title>
+              <rect className="bar" transform="translate(0,0)" y="15"></rect>
+              <rect className="bar" transform="translate(25,0)" y="15"></rect>
+              <rect className="bar" transform="translate(50,0)" y="15"></rect>
+              <rect className="bar" transform="translate(75,0)" y="15"></rect>
+              <rect className="bar" transform="translate(100,0)" y="15"></rect>
+            </g>
+          </svg>
+        </div>
         <div className="display">
           <div className="slider-music">
             <Slider
