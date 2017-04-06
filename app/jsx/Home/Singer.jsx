3c126109dda85e68c7fcf796cbca403942b13022
nguyenthaison <nguyenthaison9193@gmail.com>
2017-04-06 22:08:25 +0700

    home page continous

--- app/jsx/Home/Singer.jsx
+++ app/jsx/Home/Singer.jsx
@@ -1,26 +1,65 @@
 import TmpSinger from "./TmpSinger";
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+
+const styles = {
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    overflowY: "auto",
+  },
+};
 
 export default class Singer extends PageComponent {
   constructor(props) {
     super(props);
   }
 
-  renderListSinger() {
-    let image = "/images/headphone.jpg";
+  // renderListSinger() {
+  //   let image = "/images/headphone.jpg";
 
-    return TmpSinger.map((item, index) => {
-      return (
-        <div key={index}>
-          <div><img src={image} /></div>
-          <span>{item.name}</span>
-        </div>
-      )}
-    )
-  }
+  //   return TmpSinger.map((item, index) => {
+  //     return (
+  //       <div key={index}>
+  //         <div><img src={image} /></div>
+  //         <span>{item.name}</span>
+  //       </div>
+  //     )}
+  //   )
+  // }
 
   render() {
+    let image = "/images/headphone.jpg";
+
     return (
-      <div>{this.renderListSinger()}</div>
+      <div style={styles.root}>
+        <mui.GridList
+          cols={2}
+          cellHeight={200}
+          padding={1}
+          style={styles.gridList}
+        >
+          {TmpSinger.map((item, index) => (
+            <mui.GridTile
+              key={index}
+              title={item.name}
+              actionIcon={<mui.IconButton><StarBorder color="white" /></mui.IconButton>}
+              actionPosition="left"
+              titlePosition="top"
+              titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+              cols={1}
+              rows={4}
+            >
+              <img src={image} />
+            </mui.GridTile>
+          ))}
+        </mui.GridList>
+      </div>
     )
   }
 }
+      {/*<div>{this.renderListSinger()}</div>*/}
--- app/jsx/Home/TmpSinger.jsx
+++ app/jsx/Home/TmpSinger.jsx
@@ -1,6 +1,7 @@
 export default [
   {
     name: "ca si 1",
+    featured: true,
   },
   {
     name: "ca si 2",
@@ -10,6 +11,7 @@ export default [
   },
   {
     name: "ca si 4",
+    featured: true,
   },
   {
     name: "ca si 5",
@@ -20,22 +22,25 @@ export default [
   // {
   //   name: "ca si 1",
 
-  {
-    name: "ca si 7",
-  },
-  {
-    name: "ca si 8",
-  },
-  {
-    name: "ca si 9",
-  },
-  {
-    name: "ca si 10",
-  },
-  {
-    name: "ca si 11",
-  },
-  {
-    name: "ca si 12",
-  },
+  // {
+  //   name: "ca si 7",
+  //   featured: true,
+  // },
+  // {
+  //   name: "ca si 8",
+  // },
+  // {
+  //   name: "ca si 9",
+  //   featured: true,
+  // },
+  // {
+  //   name: "ca si 10",
+  // },
+  // {
+  //   name: "ca si 11",
+  //   featured: true,
+  // },
+  // {
+  //   name: "ca si 12",
+  // },
 ]
