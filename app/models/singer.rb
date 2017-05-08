b776d5e2f8ad090bcca7b8779e6a872c8976f6ef
nguyenthaison <nguyenthaison9193@gmail.com>
2017-05-08 13:34:04 +0700

    upload avatar admin singer page

--- app/models/singer.rb
+++ app/models/singer.rb
@@ -2,8 +2,10 @@ class Singer < ApplicationRecord
   include SmartAsJson
   include Search
 
-  ATTRIBUTES_PARAMS = [:name, :age, :dob, :content, :total_favorite, :country_id, attachment_ids: []]
-  # ALLOWED_METHODS = [:total_favorites, :background_image, :avatar_image]
+  attr_accessor :background_attachment_ids
+
+  ATTRIBUTES_PARAMS = [:name, :age, :dob, :content, :total_favorite, :country_id,
+    attachment_ids: [], background_attachment_ids: []]
   ALLOWED_METHODS = [:total_favorites]
 
   belongs_to :country
@@ -13,10 +15,14 @@ class Singer < ApplicationRecord
   has_many :songs, through: :singer_songs
   has_many :artist_music_types, as: :artist, dependent: :destroy
   has_many :attachments, as: :attachmentable, dependent: :destroy
+  has_many :background_attachments, -> {where(attachmentable_type: "background")},
+    class_name: Attachment.name, foreign_key: "attachmentable_id"
 
   validates :name, presence: true, length: {maximum: 100, minimum: 1}
   validates :dob, :country_id, presence: true
   validate :dob_must_be_before_or_equal_current_date
+
+  after_save :update_background_attachments
 
   def total_favorites
     favorite_articles.size
@@ -28,13 +34,22 @@ class Singer < ApplicationRecord
     end
   end
 
-  # def background_image
-  #   attachments.where(image_type: "background").first
-  # end
+  private
+  def update_background_attachments
+    if self.background_attachment_ids.present?
+      current_ids = self.background_attachments.pluck(:id).map {|id| id.to_s}
+      not_change_ids = current_ids & background_attachment_ids
+      add_more_ids = background_attachment_ids - not_change_ids
+      byebug
+      remove_ids = current_ids - not_change_ids
 
-  # def avatar_image
-  #   attachments.where(image_type: "avatar").first
-  # end
+      Attachment.where("id IN (?)" , add_more_ids).
+        update_all attachmentable_id: id, attachmentable_type: "background"
+
+      Attachment.where("id IN (?)" , remove_ids).
+        update_all attachmentable_id: nil
+    end
+  end
 
   class << self
     def search_by_query query
